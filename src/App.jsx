87b6d0cc07b44c54c88d0aa9c231b445a6471621
Robson Ferraz Mineiro <robson_r_f@hotmail.com>
2025-03-21 19:45:13 -0300

    chore: add tailwindcss setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import Tasks from "./Tasks"
-
 function App() {
-  return <Tasks />
+  return (
+    <div>
+      <h1 className="p-6 text-lime-500 font-bold text-9xl">Task Manager</h1>
+    </div>
+  )
 }
 export default App
