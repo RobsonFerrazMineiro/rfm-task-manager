e7382a6c21dbbce91677fbee13b872d2f0d35841
Robson Ferraz Mineiro <robson_r_f@hotmail.com>
2025-03-23 20:14:19 -0300

    feat: ensure tasks are listed on Tasks component

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,4 +1,7 @@
+import { useState } from "react"
+import TASKS from "../constants/tasks"
 import Button from "./Button"
+import TaskItem from "./TaskItem"
 import TasksSeparator from "./TasksSeparator"
 import AddIcon from "/src/assets/icons/add.svg?react"
 import CloudSunIcon from "/src/assets/icons/cloud-sun.svg?react"
@@ -7,6 +10,11 @@ import SunIcon from "/src/assets/icons/sun.svg?react"
 import TrashIcon from "/src/assets/icons/trash.svg?react"
 
 const Tasks = () => {
+  const [tasks] = useState(TASKS)
+  const morningTasks = tasks.filter((task) => task.time === "morning")
+  const afternoonTasks = tasks.filter((task) => task.time === "afternoon")
+  const nightTasks = tasks.filter((task) => task.time === "night")
+
   return (
     <div className="w-full px-8 py-16">
       <div className="flex w-full justify-between">
@@ -34,14 +42,23 @@ const Tasks = () => {
         <div className="rounded-xl bg-white p-6">
           <div className="space-y-3">
             <TasksSeparator title="Manhã" icon={<SunIcon />} />
+            {morningTasks.map((task) => (
+              <TaskItem key={task.id} task={task} />
+            ))}
           </div>
 
           <div className="my-6 space-y-3">
             <TasksSeparator title="Tarde" icon={<CloudSunIcon />} />
+            {afternoonTasks.map((task) => (
+              <TaskItem key={task.id} task={task} />
+            ))}
           </div>
 
           <div className="space-y-3">
             <TasksSeparator title="Noite" icon={<MoonIcon />} />
+            {nightTasks.map((task) => (
+              <TaskItem key={task.id} task={task} />
+            ))}
           </div>
         </div>
       </div>
---
+++ src/constants/tasks.js
@@ -0,0 +1,51 @@
+const TASKS = [
+  {
+    id: 1,
+    title: "Estudar React",
+    description:
+      "Estudar React para melhorar as habilidades em desenvolvimento web.",
+    time: "morning",
+    status: "done",
+  },
+  {
+    id: 2,
+    title: "Ler um livro",
+    description:
+      "Ler um livro sobre desenvolvimento pessoal para melhorar a qualidade de vida.",
+    time: "morning",
+    status: "done",
+  },
+  {
+    id: 3,
+    title: "Fazer exercícios",
+    description: "Ir à academia para fazer exercícios e manter a saúde em dia.",
+    time: "afternoon",
+    status: "in_progress",
+  },
+  {
+    id: 4,
+    title: "Estudar React",
+    description:
+      "Estudar React para melhorar as habilidades em desenvolvimento web.",
+    time: "afternoon",
+    status: "in_progress",
+  },
+  {
+    id: 5,
+    title: "Descansar",
+    description:
+      "Assistir a um filme ou série para relaxar e descansar a mente.",
+    time: "night",
+    status: "not_started",
+  },
+  {
+    id: 6,
+    title: "Relaxar",
+    description:
+      "Ouvir uma música ou podcast para relaxar e descansar a mente.",
+    time: "night",
+    status: "not_started",
+  },
+]
+
+export default TASKS
