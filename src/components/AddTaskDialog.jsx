14beed2e02229328f986e13d286329d364ab1a69
Robson Ferraz Mineiro <robson_r_f@hotmail.com>
2025-04-09 22:32:20 -0300

    feat: ensure AddTaskDialog can be close

--- src/components/AddTaskDialog.jsx
+++ src/components/AddTaskDialog.jsx
@@ -1,15 +1,41 @@
 import { createPortal } from "react-dom"
+import Button from "./Button"
+import Input from "./Input"
 
-const AddTaskDialog = ({ isOpen }) => {
+const AddTaskDialog = ({ isOpen, handleClose }) => {
   if (!isOpen) return null
 
   return createPortal(
     <div className="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-sm">
-      <div className="rounded-xl bg-white p-5 text-center shadow-md shadow-[#35383E]">
+      <div className="rounded-xl bg-white p-5 text-center shadow-sm shadow-[#35383E]">
         <h2 className="text-xl font-semibold text-[#35383E]">Nova Tarefa</h2>
-        <p className="mt-1 text-sm text-[#9A9C9F]">
+        <p className="mb-4 mt-1 text-sm text-[#9A9C9F]">
           Insira as informações abaixo
         </p>
+
+        <div className="flex w-[336px] flex-col space-y-4">
+          <Input id="title" label="Título" placeholder="Título da tarefa" />
+          <Input id="time" label="Horário" placeholder="Horário" />
+          <Input
+            id="description"
+            label="Descrição"
+            placeholder="Descreva a tarefa"
+          />
+
+          <div className="flex gap-3">
+            <Button
+              size="large"
+              className="w-full"
+              variant="secondary"
+              onClick={handleClose}
+            >
+              Cancelar
+            </Button>
+            <Button size="large" className="w-full">
+              Adicionar
+            </Button>
+          </div>
+        </div>
       </div>
     </div>,
     document.body
